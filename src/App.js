4046b08100ff65c7f859ab8106f4666095c5e808
iamRomeo-dev <romeo.brilland@live.fr>
2022-01-18 15:38:54 +0100

    3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>tototto</h1>>
+      <header className="App-header">
+        <p>zeoifjze fehf ezif ezoifh </p>
+      </header>
     </div>
   );
 }
